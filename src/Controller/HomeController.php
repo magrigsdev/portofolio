6a073ab42353fa737459499bd775e087f62801f4
fridev <magrigsdesign@gmail.com>
2025-03-14 13:05:11 +0100

    test page home

--- src/Controller/HomeController.php
+++ src/Controller/HomeController.php
@@ -8,18 +8,11 @@ use Symfony\Component\Routing\Attribute\Route;
 
 final class HomeController extends AbstractController
 {
-    #[Route('/home', name: 'app_home')]
+    #[Route('/', name: 'app_home')]
     public function index(): Response
     {
-        return $this->render('home/index.html.twig', [
-            'controller_name' => 'HomeController',
-        ]);
+        return $this->render('home/index.html.twig');
     }
 
-    #[Route('/test', name: 'app_home')]
-    public function test(): Response
-    {
-        return $this->render('test.html.twig');
-    }
 
 }
